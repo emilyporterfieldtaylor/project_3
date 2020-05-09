896bf5ba7844638bb74d811acf4604ad3a347a21
Kendra Kwoka <kendrakwoka@gmail.com>
2020-05-09 16:56:00 -0500

    added text to login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,19 +2,23 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import { Link } from 'react-router-dom';
 import LoginBox from '../../components/LoginBox';
-import Grid from '@material-ui/core/Grid';
+// import Grid from '@material-ui/core/Grid';
 import './style.css';
+import LoginBanner from '../../components/LoginBanner';
 
 
 function Login() {
   return (
-    <div className="loginBox">
-        <Paper >
-            <LoginBox />
-            <div>
-                <Link to='/home' style={{textDecoration: 'none'}} className="nav-link homepageLink">To Homepage (Remove once 'login' button works!)</Link>
-            </div>
-        </Paper>
+    <div>
+        <LoginBanner />
+        <div className="loginBox">
+            <Paper >
+                <LoginBox />
+                <div>
+                    <Link to='/home' style={{textDecoration: 'none'}} className="nav-link homepageLink">To Homepage (Remove once 'login' button works!)</Link>
+                </div>
+            </Paper>
+        </div>
     </div>
   );
 }
