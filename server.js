24574a624d092b03739181d46e5e04e50a33a3af
Kendra Kwoka <kendrakwoka@gmail.com>
2020-05-12 20:56:38 -0500

    removed comments

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 let gameController = require('./controllers/gameAPIController');
+
 // Requiring necessary npm packages
 var express = require("express");
 var session = require("express-session");
+
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
-var cors = require('cors')
-var convert = require('xml-js');
-let axios = require('axios');
-
+// var cors = require('cors')
+// var convert = require('xml-js');
+// let axios = require('axios');
 
 var app = express();
 var PORT = process.env.PORT || 3001;
-
 
 // Requiring models for syncing
 var db = require("./models");
@@ -20,11 +20,8 @@ var db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
 // Static directory
 app.use(express.static("public"));
-
 
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
