2f16b1abc41b29a7b55dc3bca22589c7c0e1f3c2
Kendra Kwoka <kendrakwoka@gmail.com>
2020-05-14 19:15:13 -0500

    friends list displays each friend to DOM

--- client/src/components/FriendsList/index.js
+++ client/src/components/FriendsList/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import './style.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -9,30 +12,64 @@ const useStyles = makeStyles((theme) => ({
     },
     paper: {
       padding: theme.spacing(2),
-      textAlign: 'center',
+      textAlign: 'left',
       color: theme.palette.text.secondary,
     },
 }));
 
 function FriendsList() {
     const classes = useStyles();
+        
+    const friendsList = [
+        // this will eventually get removed and call from the database to show each user's friends to the dom
+        { name: 'Kendra Kwoka'},
+        { name: 'Eric Garcia'},
+        { name: 'Caitlin Huber'},
+        { name: 'Leander Turner'},
+        { name: 'Emily Taylor'},
+      ];
 
     return (
-        <div className={classes.root} style={{marginTop: '20px'}}>
+        <div className={classes.root}>
             <Paper className={classes.paper}>
                 Friend List:
                 <ul>
-                    <li>Friend 1</li>
-                    <li>Friend 2</li>
-                    <li>Friend 3</li>
-                    <li>Friend 4</li>
-                    <li>Friend 5</li>
-                    <li>Friend 6</li>
-                    <li>Friend 7</li>
-                    <li>Friend 8</li>
-                </ul>    
+                    {friendsList.map(friend => (
+                        <li>{friend.name}</li>
+                    ))}
+                </ul>
             </Paper>
         </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // <div className={classes.root} style={{marginTop: '20px'}}>
+        //     <Paper className={classes.paper}>
+        //         Friend List:
+        //         <ul>
+        //             <li>Friend 1</li>
+        //             <li>Friend 2</li>
+        //             <li>Friend 3</li>
+        //             <li>Friend 4</li>
+        //             <li>Friend 5</li>
+        //             <li>Friend 6</li>
+        //             <li>Friend 7</li>
+        //             <li>Friend 8</li>
+        //         </ul>    
+        //     </Paper>
+        // </div>
     )
 }
 
