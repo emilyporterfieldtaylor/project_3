49614dcb791eef678c3b547da2d35bd36df069e3
Kendra Kwoka <kendrakwoka@gmail.com>
2020-05-14 22:04:29 -0500

    search BGG renders to DOM

--- client/src/components/BoardGameDescription/index.js
+++ client/src/components/BoardGameDescription/index.js
@@ -9,12 +9,12 @@ const useStyles = makeStyles((theme) => ({
     },
     paper: {
       padding: theme.spacing(2),
-      textAlign: 'center',
+      textAlign: 'left',
       color: theme.palette.text.secondary,
     },
 }));
 
-function BoardGameDescription() {
+function BoardGameDescription(props) {
     const classes = useStyles();
 
     return (
@@ -23,8 +23,11 @@ function BoardGameDescription() {
                 Board Game Description:
                
                 <Paper>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+                    <strong>Name: {props.name}</strong>
+                    <li style={{ listStyle: 'none' }}><strong>Description:</strong> {props.description} </li>
+                    <li style={{ listStyle: 'none' }}><strong>Players:</strong> {props.minPlayers} - {props.maxPlayers} </li>
+                    <li style={{ listStyle: 'none' }}><strong>PlayTime:</strong> {props.minPlayTime} - {props.maxPlayTime} </li>
+
                 </Paper>
             </Paper>
         </div>
