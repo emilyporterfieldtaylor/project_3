42fb93c0ae259e61f73df55c20b047ee35a2c78c
Emily Taylor <emilyporterfieldtaylor@gmail.com>
2020-05-18 21:55:56 -0500

    Made hot items work

--- controllers/gameAPIController.js
+++ controllers/gameAPIController.js
@@ -4,83 +4,62 @@ const db = require('../models');
 const Game = require('../models/games');
 
 const fetchXML = async (root, game) => {
-    try {
-      const response = await axios.get(`${root}${game}`);
-      return response;
-      return convert.xml2json(response.data);
-    } catch (err) {
-      return {
-        error: err.message
-      }
+  try {
+    const response = await axios.get(`${root}${game}`);
+    return convert.xml2json(response.data);
+  } catch (err) {
+    return {
+      error: err.message
     }
   }
+}
 
 // Defining methods for the booksController
 module.exports = {
-    gameController: async (req, res) => {
-        const { game } = req.params;
-        const root = 'https://www.boardgamegeek.com/xmlapi/search?search=';
-        const output = await fetchXML(root, game);
-        const json = JSON.parse(output);
-        console.log("json: ",json);
-        console.log("output: ", output);
-        if (json.errors) {
-            res.status(500);
-            res.json({
-            content: 'Unable to get the data from boardgamegeek.com',
-            ... json
-            })
-        } else {
-            res.json(json);
-        }
-    },
-    findById:  async (req, res) => {
-        const { id } = req.params;
-        const root = 'https://www.boardgamegeek.com/xmlapi2/thing?id=';
-        const output = await fetchXML(root, id);
-        const json = JSON.parse(output);
-        // console.log("json: ",json);
-        // console.log("output: ", output);
-        if (json.errors) {
-            res.status(500);
-            res.json({
-            content: 'Unable to get the data from boardgamegeek.com',
-            ... json
-            })
-        } else {
-            res.json(json);
-        }
-    },
-    create: function(req, res) {
-      // console.log('in the controller');
-      let gameData = {
-        gameId: req.body.gameId,
-        name: req.body.name,
-        yearPublished: req.body.yearPublished,
-        description: req.body.description,
-        minPlayers: req.body.minPlayers,
-        maxPlayers: req.body.maxPlayers,
-        minPlayTime: req.body.minPlayTime,
-        maxPlayTime: req.body.maxPlayTime,
-        yearPublished: req.body.yearPublished,
-      }
-      // console.log("controller data: ", gameData)
-      db.Game.create(gameData)
+  gameController: async (req, res) => {
+    const { game } = req.params;
+    const root = 'https://www.boardgamegeek.com/xmlapi/search?search=';
+    const output = await fetchXML(root, game);
+    const json = JSON.parse(output);
+    // console.log("json: ",json);
+    // console.log("output: ", output);
+    if (json.errors) {
+      res.status(500);
+      res.json({
+        content: 'Unable to get the data from boardgamegeek.com',
+        ...json
+      })
+    } else {
+      res.json(json);
+    }
+  },
+  findById: async (req, res) => {
+    const { id } = req.params;
+    const root = 'https://www.boardgamegeek.com/xmlapi2/thing?id=';
+    const output = await fetchXML(root, id);
+    const json = JSON.parse(output);
+    // console.log("json: ",json);
+    // console.log("output: ", output);
+    if (json.errors) {
+      res.status(500);
+      res.json({
+        content: 'Unable to get the data from boardgamegeek.com',
+        ...json
+      })
+    } else {
+      res.json(json);
+    }
+  },
+  create: function (req, res) {
+    db.Game
       // call db to connect with database, then exported variable from model file
-        // .create(req.body)
-        // console.log(gameData)
-        .then(game => {
-          res.json({status: game.name + ' successfully entered into database!'});
-        })
-        .catch(err => {
-          res.send('controller error: ' + err)
-        })
-          // console.log(err)); 
-          // res.status(422).json(err));
-    },
-    hotItems: async (res) => {
-      const root = 'https://boardgamegeek.com/xmlapi2/hot?boardgame';
-      const output = await fetchXML(root);
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  hotItems: async (req, res) => {
+    const root = 'https://boardgamegeek.com/xmlapi2/hot?type=boardgame';
+    const output = await fetchXML(root);
       const json = JSON.parse(output);
       console.log("json: ",json);
       console.log("output: ", output);
@@ -93,6 +72,6 @@ module.exports = {
     } else {
         res.json(json);
     }
-    }
+  }
 };
 
