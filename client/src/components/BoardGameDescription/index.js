05792528adfe63caea25cc5ddd4e6633625b5d38
Leandra Turner <calliet0715@gmail.com>
2020-05-22 18:44:54 -0500

    Adding load game function to render games back to homepage

--- client/src/components/BoardGameDescription/index.js
+++ client/src/components/BoardGameDescription/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import './style.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import API from '../../utils/index.js';
