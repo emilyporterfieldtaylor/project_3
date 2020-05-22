0cc44fc6de1beb599dc485c718af5a131da1a691
Leandra Turner <calliet0715@gmail.com>
2020-05-22 18:42:35 -0500

    commenting out console log for state.savedGames

--- client/src/components/BoardGameList/index.js
+++ client/src/components/BoardGameList/index.js
@@ -29,26 +29,26 @@ function BoardGameList() {
         loadGames()
     }, [])
 
-    
+    //part of associating games to a specific user
     function loadGames() {
         API.getUserGames().then(results=>{
-            console.log("Mygames",results.data)
+            console.log("My games",results.data)
             dispatch({type: "GET_USER_GAMES", games: results.data })
         })
     }
 
 
-    // const userSavedGames = [
-    //     // this will eventually be deleted
-    //     // info should be pulled from database of a user's saved games 
-    //     { title: 'Settlers of Catan', year: 1995 },
-    //     { title: 'Crossbows and Catapults', year: 1983 },
-    //     { title: 'Cards Against Humanity', year: 2009 },
-    //     { title: 'Exploding Kittens', year: 2015 },
-    //     { title: 'Scattergories', year: 1988 },
-    //     { title: "Magic: The Gathering", year: 1993 },
-    //     { title: 'Photosynthesis', year: 2017 },
-    // ];
+     const userSavedGames = [
+         // this will eventually be deleted
+         // info should be pulled from database of a user's saved games 
+         { title: 'Settlers of Catan', year: 1995 },
+         { title: 'Crossbows and Catapults', year: 1983 },
+         { title: 'Cards Against Humanity', year: 2009 },
+         { title: 'Exploding Kittens', year: 2015 },
+         { title: 'Scattergories', year: 1988 },
+         { title: "Magic: The Gathering", year: 1993 },
+         { title: 'Photosynthesis', year: 2017 },
+     ];
 
 
     return (
@@ -56,6 +56,7 @@ function BoardGameList() {
             <Paper className={classes.paper}>
                 Board Game List:
                 <ul className={classes.boardgameUL}>
+                    {/* {console.log(state.savedGames, "saved-games")} */}
                     {state.savedGames.map(game => (
                         //pulling games from the database and rendering to the homepage
                         <li key={game.id}>{game.name} ({game.yearPublished})</li>
