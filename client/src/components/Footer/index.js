2f515ebce14f71adeaf4e5e6cd564569848568cc
Orchid23 <calliet0715@gmail.com>
2020-05-23 21:22:10 -0500

    Creating new component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+
+
+function Footer() {
+    return (
+        <div className="sticky-footer">
+            copyright &copy;
+        </div>
+    )
+}
+
+export default Footer;
