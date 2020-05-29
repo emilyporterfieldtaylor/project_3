7bbf941321ef3922e3540f69d363a5b04b026df2
Leandra Turner <calliet0715@gmail.com>
2020-05-29 09:38:50 -0500

    validation for password length

--- client/src/components/SignupForm/index.js
+++ client/src/components/SignupForm/index.js
@@ -13,13 +13,7 @@ import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff'
 import './signup.css';
 import API from '../../utils/index';
-<<<<<<< HEAD
-
-
-=======
-import { useHistory } from "react-router-dom"
 import { useStoreContext } from '../../utils/GlobalState';
->>>>>>> origin/kkStyling
 //material ui code for input boxes
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -73,18 +67,16 @@ export default function SignupForm() {
             email: values.email,
             password: values.password
         }
-
+        if (values.password.length < 8) {
+            alert("Passwords must be at lease 8 characters")
+        } else {
         API.signup(userData).then(results => {
-<<<<<<< HEAD
+            { dispatch({ type: "ADD_USERDATA", data: results.data }) }
             //console.log(results)
-=======
-            { dispatch({type: "ADD_USERDATA", data: results.data }) }
-            console.log(results)
->>>>>>> origin/kkStyling
             history.push("/hotitems");
             //console.log("words");
         }).catch(loginError)
-
+    }
 
     }
 
@@ -99,61 +91,59 @@ export default function SignupForm() {
             alert('Oops one of the fields was left blank, please fully complete form.');
         } else if (values.email === values.email) {
             alert("User already exists, use login page to continue");
-        } else if (values.password < 8 || values.password > 128) {
-            alert("Please select a password between 8 and 128 characters")
         }
     }
 
-        return (
-            <div className="signup-main">
-                <img className="logo" src="/images/ALaBoardLogo1.png" alt="game logo" />
-                <div className="signup-frame">
-                    <Grid item xs={12}>
-                        <h2 className="signup-h2">Join the party!</h2>
-                        <div className={classes.root}>
-                            <div>
-                                <TextField
-                                    label="Full Name"
-                                    id="filled-start-adornment"
-                                    className={clsx(classes.margin, classes.textField)}
-                                    variant="filled"
-                                    onChange={handleChange('name')}
+    return (
+        <div className="signup-main">
+            <img className="logo" src="/images/ALaBoardLogo1.png" alt="game logo" />
+            <div className="signup-frame">
+                <Grid item xs={12}>
+                    <h2 className="signup-h2">Join the party!</h2>
+                    <div className={classes.root}>
+                        <div>
+                            <TextField
+                                label="Full Name"
+                                id="filled-start-adornment"
+                                className={clsx(classes.margin, classes.textField)}
+                                variant="filled"
+                                onChange={handleChange('name')}
+                            />
+                            <TextField
+                                label="Email"
+                                id="filled-start-adornment"
+                                className={clsx(classes.margin, classes.textField)}
+                                variant="filled"
+                                onChange={handleChange('email')}
+                            />
+                            <FormControl className={clsx(classes.margin, classes.textField)} variant="filled">
+                                <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
+                                <FilledInput
+                                    id="filled-adornment-password"
+                                    type={values.showPassword ? 'text' : 'password'}
+                                    value={values.password}
+                                    onChange={handleChange('password')}
+                                    endAdornment={
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={handleClickShowPassword}
+                                                onMouseDown={handleMouseDownPassword}
+                                                edge="end"
+                                            >
+                                                {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    }
                                 />
-                                <TextField
-                                    label="Email"
-                                    id="filled-start-adornment"
-                                    className={clsx(classes.margin, classes.textField)}
-                                    variant="filled"
-                                    onChange={handleChange('email')}
-                                />
-                                <FormControl className={clsx(classes.margin, classes.textField)} variant="filled">
-                                    <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
-                                    <FilledInput
-                                        id="filled-adornment-password"
-                                        type={values.showPassword ? 'text' : 'password'}
-                                        value={values.password}
-                                        onChange={handleChange('password')}
-                                        endAdornment={
-                                            <InputAdornment position="end">
-                                                <IconButton
-                                                    aria-label="toggle password visibility"
-                                                    onClick={handleClickShowPassword}
-                                                    onMouseDown={handleMouseDownPassword}
-                                                    edge="end"
-                                                >
-                                                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                                                </IconButton>
-                                            </InputAdornment>
-                                        }
-                                    />
-                                </FormControl>
-                            </div>
+                            </FormControl>
                         </div>
-                        <button type="submit" className="btn btn-default" onClick={handleFormSubmit}><a href="/hotitems" >Sign Up</a></button>
-                        <br />
-                        <p>Already have an account?<Link className="login-link" to="/login"> LOGIN </Link></p>
-                    </Grid>
-                </div>
+                    </div>
+                    <button type="submit" className="btn btn-default" onClick={handleFormSubmit}><a href="/hotitems" >Sign Up</a></button>
+                    <br />
+                    <p>Already have an account?<Link className="login-link" to="/login"> LOGIN </Link></p>
+                </Grid>
             </div>
-        )
-    };
+        </div>
+    )
+};
