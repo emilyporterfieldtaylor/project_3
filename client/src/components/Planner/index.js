af67809f08c7498009f80d10eecdbf671647648b
Kendra Kwoka <kendrakwoka@gmail.com>
2020-05-28 19:34:54 -0500

    pushing to heroku

--- client/src/components/Planner/index.js
+++ client/src/components/Planner/index.js
@@ -14,7 +14,12 @@ const useStyles = makeStyles((theme) => ({
   outerdiv: {
     border: 'solid 1px grey',
     backgroundColor: 'whitesmoke',
-    margin: '20px'
+    margin: '20px',
+    fontFamily: 'Pangolin',
+    borderRadius: '10px'
+  },
+  hiddenLI: {
+    listStyleType: 'none'
   }
 }));
 
@@ -23,17 +28,17 @@ export default function Planner () {
   const [gameCategory, setGameCategory] = useState([]);
   const classes = useStyles();
 
-  function applyPlayerNumber(e) {
-    e.preventDefault();
-    console.log('in function: ',playerInput)
-    // api call for number of players
-  }
+  // function applyPlayerNumber(e) {
+  //   e.preventDefault();
+  //   console.log('in function: ',playerInput)
+  //   // api call for number of players
+  // }
 
-  function applyGameCategory(e) {
-    e.preventDefault();
-    console.log('game type: ')
-    // api call for categories
-  }
+  // function applyGameCategory(e) {
+  //   e.preventDefault();
+  //   console.log('game type: ')
+  //   // api call for categories
+  // }
 
   const handlePlayerChange = (e) => {
     setPlayerInput(e.target.value);
@@ -43,13 +48,12 @@ export default function Planner () {
     setGameCategory(e.target.value);
   }
 
-  console.log('outside function: ',playerInput)
   return (
     <div className={classes.outerdiv}>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={6} md={6}>
+        <Grid item xs={12} sm={6} md={6} className='eventPageGrid'>
           <form className={classes.root} noValidate autoComplete="off">
-            <h4>Number of players: {playerInput}</h4>
+            <h4>Number of players: </h4>
             <TextField 
               id="outlined-basic" 
               label="Players" 
@@ -57,7 +61,7 @@ export default function Planner () {
               size='small'
               onChange={handlePlayerChange}
               />
-            <button onClick={(e) => {applyPlayerNumber(e)}}>Apply!</button>
+            {/* <button onClick={(e) => {applyPlayerNumber(e)}}>Apply!</button> */}
             
             <h4>Category of games:</h4>
             <TextField 
@@ -67,19 +71,16 @@ export default function Planner () {
               size='small'
               onChange={handleGameChange}
               />
-            <button onClick={(e) => {applyGameCategory(e)}}>Apply!</button>
-            
-              <h4>How many players would you like to have?</h4>
-            <TextField id="outlined-basic" label="numberPlayers" variant="outlined" />
+            {/* <button onClick={(e) => {applyGameCategory(e)}}>Apply!</button> */}
+          
           </form>
         </Grid>
-        <Grid item xs={12} sm={6} md={6}>
+        <Grid item xs={12} sm={6} md={6} className='eventPageGrid'>
           <h3>You'd like to create an event for:</h3>
           <ul>
-            <li> <b>{playerInput}</b> player(s) </li>
-            <li> <b>{gameCategory}</b> type </li>
+            <li className={classes.hiddenLI}> <b>{playerInput}</b> player(s) </li>
+            <li className={classes.hiddenLI}> <b>{gameCategory}</b> type </li>
           </ul>
-          {playerInput} player
         </Grid>
       </Grid>
     
--- client/src/pages/PlanEvent/index.js
+++ client/src/pages/PlanEvent/index.js
@@ -9,7 +9,7 @@ export default function PlanEvent () {
         <div id='eventPage'>
             <Link to='/home'>To the Bulletin Board</Link>
             <Header />
-            <h1 className="bulletin">Games, Nerds, Shindigs</h1>
+            <h1 className="bulletin2">Games, Nerds, Shindigs</h1>
             <Planner />
         </div>
     )
