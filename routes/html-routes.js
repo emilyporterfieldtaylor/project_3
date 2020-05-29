128e8c8aa56346e38dcae2c8e0ff7793a0f32499
Leandra Turner <calliet0715@gmail.com>
2020-05-29 10:17:00 -0500

    Adding comments

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -33,7 +33,7 @@ module.exports = function(app) {
   // })
 
   // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  // If a user, who is not logged, in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
--- routes/utils/routeHelper.js
+++ routes/utils/routeHelper.js
@@ -1,3 +1,4 @@
+//Using for oauth
 module.exports = () => {
   if(!process.env.NODE_ENV) {
       return process.env.REACT_APP_DEV_URL_FRONTEND;
