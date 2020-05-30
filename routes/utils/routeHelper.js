bb6291881410ebf5858f31f086b4ba62a7652067
Emily Taylor <emilyporterfieldtaylor@gmail.com>
2020-05-30 09:49:55 -0500

    change link on logingbox

--- routes/utils/routeHelper.js
+++ routes/utils/routeHelper.js
@@ -1,8 +1,8 @@
 //Using for oauth
 module.exports = () => {
-  // if(!process.env.NODE_ENV) {
-  //     return process.env.REACT_APP_DEV_URL_FRONTEND;
+  if(!process.env.NODE_ENV) {
+      return process.env.REACT_APP_DEV_URL_FRONTEND;
 
-  // }
+  }
   return process.env.REACT_APP_PROD_URL;
 }
