78f5284881c38209240a763bfff14499bfc8d373
Leandra <53972257+lturner19@users.noreply.github.com>
2020-05-30 01:56:15 -0500

    Resetting files

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -43,8 +43,5 @@ export default {
   },
   searchThruGames: function() {
     return axios.get('/api/search_thru_games')
-  },
-  updateFirstTimeLogin: function() {
-    return axios.put('/api/firstlogin')
   }
 };
